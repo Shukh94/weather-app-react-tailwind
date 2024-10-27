88190af7616d5f69a31ef67a95ff8b88345915a7
Shukh94 <shakila.sk09@gmail.com>
2024-10-27 18:24:57 +0600

    added buttons and input components with css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Inputs from "./components/Inputs";
+import TopButtons from "./components/TopButtons";
 
 const App = () => {
   return (
-    <div>
-       <h1 className="text-7xl text-red-400 ">
-      Hello world!
-    </h1>
+    <div className=" mx-auto items-center max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br shadow-xl shadow-gray-400 from-cyan-700 to-blue-700">
+      <TopButtons />
+      <Inputs />
     </div>
   )
 }
